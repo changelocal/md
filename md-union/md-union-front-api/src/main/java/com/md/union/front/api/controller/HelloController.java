d45096bb832812c8544bb4d08f21fc2e99acda44
shenxiaojian <shenxiaojian@travelsky.com>
2020-07-06 16:28:40 +0800

    add api

--- md-union/md-union-front-api/src/main/java/com/md/union/front/api/controller/HelloController.java
+++ md-union/md-union-front-api/src/main/java/com/md/union/front/api/controller/HelloController.java
@@ -1,6 +1,9 @@
 package com.md.union.front.api.controller;
 
 
+import com.arc.common.ServiceException;
+import com.arc.util.http.BaseResponse;
+import com.md.union.front.client.dto.SampleDTO;
 import com.md.union.front.client.feign.FrontClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
