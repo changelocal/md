e37f3a60fbc1aa8f54622e69cb77861124929efc
shenxiaojian <shenxiaojian@travelsky.com>
2020-07-25 14:35:55 +0800

    add api

--- md-union/md-union-front-api/src/main/java/com/md/union/front/api/controller/ResourceController.java
+++ md-union/md-union-front-api/src/main/java/com/md/union/front/api/controller/ResourceController.java
@@ -1,10 +1,9 @@
 package com.md.union.front.api.controller;
 
+import com.md.union.front.api.vo.Brand;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,5 +24,13 @@ public class ResourceController {
         result.add("https://www.jseea.cn/contents/channel_178/2018/11/1811211458927(3).png");
         result.add("https://www.jseea.cn/contents/channel_178/2018/11/1811211458927(3).png");
         return result;
+    }
+
+    @ApiOperation("上传图片")
+    @PostMapping("/uploadPic")
+    public String brandSearch(@RequestBody Brand.SearchReq request) {
+
+
+        return "";
     }
 }
