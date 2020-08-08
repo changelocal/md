947b3f11434b4eb594ba844c27d5bb9cb3fd3828
tianxiuquan <tianxiuquan@126.com>
2020-08-08 10:05:19 +0800

    修改

--- md-union/md-union-front-api/src/main/java/com/md/union/front/api/Enums/ChangeEnums.java
+++ md-union/md-union-front-api/src/main/java/com/md/union/front/api/Enums/ChangeEnums.java
@@ -10,21 +10,21 @@ public enum ChangeEnums {
 
 
 
-    BRAND_REFUSE_DEAL(1, "商标转让", "商标权发生改变，依照商标法规定须向商标局提", 700,""),
-    BRAND_DISCUSS_DEAL(2, "商标宽展", "商标到期后6个月希望继续保有该商标，须向", 1200,""),
-    BRAND_CANCEL_DEAL(3, "商标续展", "保护商标品牌，延长有效期", 980,""),
-    BRAND_OBJECTTION_DEAL(4, "商标补证", "商标注册证遗失或破损，即刻为您申请补发证书", 800,""),
-    BRAND_MONITOR_DEAL(5, "商标更正", "如注册申报时公司名称或地址填写错误等，须向", 400,""),
-    BRAND_APPEAL_DEAL(6, "商标注销", "商标持有人放弃全部或部分商标权，须向商标局提出", 300,""),
-    BRAND_INVALID_DEAL(7, "商标变更", "如需变更注册人名义，等级地址等注册事项，需向", 350,""),
+    BRAND_REFUSE_DEAL(1, "商标转让", "商标权发生改变，依照商标法规定须向商标局提", 700,"http://47.92.65.35:8082/file/brand-change/1.png"),
+    BRAND_DISCUSS_DEAL(2, "商标宽展", "商标到期后6个月希望继续保有该商标，须向", 1200,"http://47.92.65.35:8082/file/brand-change/2.png"),
+    BRAND_CANCEL_DEAL(3, "商标续展", "保护商标品牌，延长有效期", 980,"http://47.92.65.35:8082/file/brand-change/3.png"),
+    BRAND_OBJECTTION_DEAL(4, "商标补证", "商标注册证遗失或破损，即刻为您申请补发证书", 800,"http://47.92.65.35:8082/file/brand-change/4.png"),
+    BRAND_MONITOR_DEAL(5, "商标更正", "如注册申报时公司名称或地址填写错误等，须向", 400,"http://47.92.65.35:8082/file/brand-change/5.png"),
+    BRAND_APPEAL_DEAL(6, "商标注销", "商标持有人放弃全部或部分商标权，须向商标局提出", 300,"http://47.92.65.35:8082/file/brand-change/6.png"),
+    BRAND_INVALID_DEAL(7, "商标变更", "如需变更注册人名义，等级地址等注册事项，需向", 350,"http://47.92.65.35:8082/file/brand-change/7.png"),
     ;
 
-    ChangeEnums(int type, String title, String brief, int price, String img) {
+    ChangeEnums(int type, String title, String brief, int price, String icon) {
         this.type = type;
         this.title = title;
         this.brief = brief;
         this.price = price;
-        this.img = img;
+        this.icon = icon;
     }
 
     public int getType() {
@@ -43,8 +43,8 @@ public enum ChangeEnums {
         return price;
     }
 
-    public String getImg() {
-        return img==null?def_img:img;
+    public String getIcon() {
+        return icon;
     }
 
     private String def_img = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1595638504208&di=754606fdcb88e6b4ee9ea1476f4c2a5f&imgtype=0&src=http%3A%2F%2Fimage-ali.bianjiyi.com%2F1%2F2018%2F0710%2F14%2F15312043093853.jpg";
@@ -59,7 +59,7 @@ public enum ChangeEnums {
     //价格
     private int price;
     //图片
-    private String img;
+    private String icon;
 
     public static ChangeEnums valueType(int id) {
         for (ChangeEnums item : ChangeEnums.values()) {
--- md-union/md-union-front-api/src/main/java/com/md/union/front/api/Enums/DealEnums.java
+++ md-union/md-union-front-api/src/main/java/com/md/union/front/api/Enums/DealEnums.java
@@ -10,29 +10,29 @@ public enum DealEnums {
 
 
 
-    BRAND_REFUSE_DEAL(1,"商标驳回复审","商标注册被驳回后的唯一救济方式",1800,""),
+    BRAND_REFUSE_DEAL(1,"商标驳回复审","商标注册被驳回后的唯一救济方式",1800,"http://47.92.65.35:8082/file/brand-power/1.png"),
 
-    BRAND_DISCUSS_DEAL(2,"商标异议答辩","商标在公示期内被他人提出异议，可以申请异议答辩",2000,""),
+    BRAND_DISCUSS_DEAL(2,"商标异议答辩","商标在公示期内被他人提出异议，可以申请异议答辩",2000,"http://47.92.65.35:8082/file/brand-power/2.png"),
 
-    BRAND_CANCEL_DEAL(3,"商标撤三申请","撤销他人三年不使用商标，扫清障碍",1500,""),
+    BRAND_CANCEL_DEAL(3,"商标撤三申请","撤销他人三年不使用商标，扫清障碍",1500,"http://47.92.65.35:8082/file/brand-power/3.png"),
 
-    BRAND_OBJECTTION_DEAL(4,"商标异议申请","异议公示期近似商标，阻止其注册成功，维护品牌权益",1500,""),
+    BRAND_OBJECTTION_DEAL(4,"商标异议申请","异议公示期近似商标，阻止其注册成功，维护品牌权益",1500,"http://47.92.65.35:8082/file/brand-power/4.png"),
 
-    BRAND_MONITOR_DEAL(5,"侵权监测","拒绝搭便车，蹭热度，多个类别同时查询",2300,""),
+    BRAND_MONITOR_DEAL(5,"侵权监测","拒绝搭便车，蹭热度，多个类别同时查询",2300,"http://47.92.65.35:8082/file/brand-power/5.png"),
 
-    BRAND_APPEAL_DEAL(6,"电商申诉","针对店铺品牌，产品被投诉，侵权，提供一站式申诉服务",1600,""),
+    BRAND_APPEAL_DEAL(6,"电商申诉","针对店铺品牌，产品被投诉，侵权，提供一站式申诉服务",1600,"http://47.92.65.35:8082/file/brand-power/6.png"),
 
-    BRAND_INVALID_DEAL(7,"商标无效宣告","宣告他人商标无效，扫清障碍",1200,""),
+    BRAND_INVALID_DEAL(7,"商标无效宣告","宣告他人商标无效，扫清障碍",1200,"http://47.92.65.35:8082/file/brand-power/7.png"),
 
-    BRAND_PERMIT_DEAL(8,"商标许可备案","不放弃商标权，但允许他人使用该商标",1200,""),
+    BRAND_PERMIT_DEAL(8,"商标许可备案","不放弃商标权，但允许他人使用该商标",1200,"http://47.92.65.35:8082/file/brand-power/8.png"),
             ;
 
-    DealEnums(int type, String title, String brief, int price, String img) {
+    DealEnums(int type, String title, String brief, int price, String icon) {
         this.type = type;
         this.title = title;
         this.brief = brief;
         this.price = price;
-        this.img = img;
+        this.icon = icon;
     }
 
     public int getType() {
@@ -51,8 +51,8 @@ public enum DealEnums {
         return price;
     }
 
-    public String getImg() {
-        return ((img == null)||img.length()==0) ? def_img : img;
+    public String getIcon() {
+        return icon;
     }
 
     private String def_img = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1595638504208&di=754606fdcb88e6b4ee9ea1476f4c2a5f&imgtype=0&src=http%3A%2F%2Fimage-ali.bianjiyi.com%2F1%2F2018%2F0710%2F14%2F15312043093853.jpg";
@@ -66,7 +66,7 @@ public enum DealEnums {
     //价格
     private int price;
     //图片
-    private String img;
+    private String icon;
 
     public static DealEnums valueType(int id) {
         for (DealEnums item : DealEnums.values()) {
